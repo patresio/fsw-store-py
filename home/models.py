b56ce2f79c58a0e157d9e7ad80c409a6c071a749
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2023-10-28 21:12:26 -0300

    feat: migrate products and models

--- home/models.py
+++ home/models.py
@@ -1,4 +1,6 @@
+from itertools import product
 from tabnanny import verbose
+from tkinter import CASCADE
 
 from django.db import models
 from django.shortcuts import resolve_url as r
@@ -6,7 +8,7 @@ from django.shortcuts import resolve_url as r
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField('name', max_length=100, blank=True)
+    name = models.CharField('nome', max_length=100, blank=True)
     slug = models.SlugField('slug')
     imageUrl = models.CharField('image', max_length=255)
 
@@ -17,5 +19,45 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.nome)
+        return super().save()
+
     def get_absolute_url(self):
         return r('speaker_detail', slug=self.slug)
+
+
+class Product(models.Model):
+    name = models.CharField('nome', max_length=255, blank=True)
+    slug = models.SlugField('slug')
+    description = models.TextField('descrição')
+    basePrice = models.DecimalField('preço base', max_digits=7, decimal_places=2)
+    categoryId = models.ForeignKey('Category', verbose_name='categoria', on_delete=models.CASCADE)
+    discountPercentage = models.IntegerField('desconto')
+
+    
+    class Meta:
+        verbose_name='produto'
+        verbose_name_plural='produtos'
+
+    def __str__(self):
+        self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.nome)
+        return super().save()
+    
+    def get_absolute_url(self):
+        return r('speaker_detail', slug=self.slug)
+    
+    def totalPrice(self):
+        if self.discountPercentage > 0:
+            self.total_price = (self.basePrice - (self.basePrice * (self.discountPercentage/100)))
+        return self.total_price
+    
+
+class ImageProduct(models.Model):
+    imageUrl = models.TextField('image')
+    product = models.ForeignKey('Product', verbose_name='produto', on_delete=models.CASCADE)
